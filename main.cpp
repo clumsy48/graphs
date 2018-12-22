f6c1e1b7f2a9477ff3502e63fa5072c6c86be78c
Krrishna Murari <m007urray@gmail.com>
2018-12-22 19:02:31 +0530

    matrix representation

---
+++ main.cpp
@@ -0,0 +1,41 @@
+/*#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+}*/
+/***********************************************************************************************
+ * 
+ * Undirected GRAPH 
+ * REPRESENTATION USING VECTOR ARRAY
+ * 
+ * *******************************************************************************************/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void addEdge(vector<int> graph[],int src ,int dest){
+           graph[src].push_back(dest);
+           graph[dest].push_back(src);
+}
+
+int main(){
+
+    int vecticesCount ,edgeCount;
+    cin >> vecticesCount;
+
+    vector<int> graph[vecticesCount];
+    cout << "enter edges count :";
+    cin >> edgeCount;
+    cout << "enter edges (ex- source_vectex dest_vertex)\n";
+    for(int i=0 ; i<edgeCount ;i++){
+        int src ,dest;
+        cin >> src >> dest;
+        addEdge(graph,src-1,dest-1);
+    }
+}
+
