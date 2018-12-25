1542479419090b15b4246099b4fd1aab4d1fdaaf
Krrishna Murari <m007urray@gmail.com>
2018-12-25 10:46:52 +0530

    connected components

--- Graphs/dfs/connectivity.cpp
+++ Graphs/dfs/connectivity.cpp
@@ -1,5 +1,5 @@
 /********************************
- * 
+ * FINDING CONNCTED COMPONENTS
  * UNDIRECTED GRAPH
  *  (0, 1) 
     (0, 3) 
@@ -14,38 +14,48 @@
 #include <iostream>
 #include <vector>
 
+
 using namespace std;
+
+static int counter =0;
+vector<int> component;
 
 void addEdge(vector<int> graph[], int src, int dest)
 {
     graph[src].emplace_back(dest);
 }
-void dfsUtil(vector<int> graph[], int src, vector<bool> &visited)
+void dfsUtil(vector<int> graph[], int src, vector<bool> &visited,int c)
 {
     visited[src] = true;
-    cout << src << "\n";
+    component[src] = c;
+   
     for (vector<int>::iterator i = graph[src].begin(); i != graph[src].end(); i++)
     {
         if (visited[*i] == false)
-            dfsUtil(graph, *i, visited);
+            dfsUtil(graph, *i, visited,c);
     }
 }
-void dfs(vector<int> graph[], int src, int vecticesCount)
+void dfs(vector<int> graph[], int src, int verticesCount)
 {
-    vector<bool> visited(vecticesCount, false);
-    for (int i = 0; i < vecticesCount; i++)
+    vector<bool> visited(verticesCount, false);
+    component = vector<int>(verticesCount,-1);
+    for (int i = 0; i < verticesCount; i++)
     {
-        if (visited[i] == false)
-            dfsUtil(graph, i, visited);
+        if (visited[i] == false){
+            dfsUtil(graph, i,visited, ++counter);
+            }
+    }
+    for(int i=0;i<verticesCount;i++){
+        cout << i << " " << component[i] <<endl;
     }
 }
 
 int main()
 {
 
-    int vecticesCount = 9, edgeCount = 8;
+    int verticesCount = 9, edgeCount = 8;
 
-    vector<int> graph[vecticesCount];
+    vector<int> graph[verticesCount];
 
     addEdge(graph, 0, 1);
     addEdge(graph, 0, 3);
@@ -55,6 +65,7 @@ int main()
     addEdge(graph, 4, 6);
     addEdge(graph, 5, 6);
     addEdge(graph, 7, 8);
-
-    dfs(graph, 2, vecticesCount);
+    counter = 0;
+    dfs(graph, 2, verticesCount);
+    
 }
