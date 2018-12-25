536eff151a5ae24d3b021178a4ae55af1440bf6e
Krrishna Murari <m007urray@gmail.com>
2018-12-25 16:21:04 +0530

    topologival sorting

--- Graphs/dfs/topologicalsort_using_stack.cpp
+++ Graphs/dfs/topologicalsort_using_stack.cpp
@@ -1,32 +1,23 @@
 /********************************
  * TOPOLOGICAL SORT
  * DIRECTED GRAPH
-    0, 1;
-     0, 3;
-     0, 4;
-     1, 2;
-     2, 3;
-     4, 3;
+ *  (0, 1); 
+    (0, 2); 
+    (1, 2); 
+    (2, 0); 
+    (2, 3); 
+    (3, 3);
  * 
  * ***********************************/
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <stack>
 
 using namespace std;
-struct post_order{
-    int vertex;
-    int ordering;
-};
 
-static int counter = 0;
-vector<int> pre_order;
-vector<struct post_order> post_order;
+stack<int> post_order;
 
-bool mycompator(struct post_order l,struct post_order r){
-      return l.ordering>r.ordering;
-}
 
 void addEdge(vector<int> graph[], int src, int dest)
 {
@@ -35,25 +26,23 @@ void addEdge(vector<int> graph[], int src, int dest)
 void dfsUtil(vector<int> graph[], int src, vector<bool> &visited)
 {
     visited[src] = true;
-    pre_order[src] = ++counter;
-    cout << src << " " << counter << "\n";
+
     for (vector<int>::iterator i = graph[src].begin(); i != graph[src].end(); i++)
     {
         if (visited[*i] == false)
             dfsUtil(graph, *i, visited);
     }
-    post_order[src] = {src,++counter};
+    post_order.push(src);
 }
 void dfs(vector<int> graph[], int src, int verticesCount)
 {
     vector<bool> visited(verticesCount, false);
-    pre_order = vector<int>(verticesCount);
-    post_order = vector<struct post_order>(verticesCount);
+   
     dfsUtil(graph, src, visited);
     cout << "-----------\nlinear ordering: \n";
-    sort(post_order.begin(),post_order.end(),mycompator);
-    for(int i=0;i<post_order.size();i++){
-       cout << post_order[i].vertex << " ";
+    while(!post_order.empty()){
+        cout << post_order.top() << " ";
+        post_order.pop(); 
     }
     cout << endl;
 }
