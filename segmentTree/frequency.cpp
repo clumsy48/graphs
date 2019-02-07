369a6a8942e3fe29591ac21335a67e411bfb8d84
Krrishna Murari <m007urray@gmail.com>
2019-02-07 21:53:44 +0530

    SEG T frequency update

--- segmentTree/frequency.cpp
+++ segmentTree/frequency.cpp
@@ -1,5 +1,5 @@
 /**********************************
- * minimum QUERY
+ * Frequency QUERY
  * *****************/
 
 #include <iostream>
@@ -12,56 +12,56 @@ using namespace std;
     cin.tie(0)
 struct stNode
 {
-    int fqLtMost, fqRtMost, fqMax;
+    ll fqLtMost, fqRtMost, fqMax;
 };
-
-struct stNode getNodeLR(struct stNode *st, struct stNode lc, struct stNode rc)
-{
-    struct stNode temp;
-
-    temp.prefix = max(lc.prefix, lc.sum + rc.prefix);
-    temp.suffix = max(rc.suffix, rc.sum + lc.suffix);
-    temp.sum = rc.sum + lc.sum;
-    temp.maxsum = max(temp.suffix, max(temp.prefix, max(rc.maxsum, max(max(lc.maxsum, rc.maxsum), lc.suffix + rc.prefix))));
-    return temp;
-}
-struct stNode RMQ(int *arr,struct stNode *st, ll cur, ll ql, ll qr, ll l, ll r)
-{
-    struct stNode temp;
-    temp.fqLtMost = temp.fqRtMost = temp.fqMax = 0;
-    if (l > qr || r < ql)
-        return temp;
-    if (l >= ql && r <= qr)
-    {
-        return st[cur];
-    }
-    ll m = (l + r) / 2;
-    if(ql>m) return RMQ(arr,st, 2 * cur + 2, ql, qr, m + 1, r);
-    else if(qr<m) return RMQ(arr,st, 2 * cur + 1, ql, qr, l, m);
-    struct stNode lst = RMQ(arr,st, 2 * cur + 1, ql, qr, l, m);
-    struct stNode rst = RMQ(arr,st, 2 * cur + 2, ql, qr, m + 1, r);
-   /* if (lst.fqMax == 0)
-        return rst;
-    if (rst.fqMax == 0)
-        return lst;*/
-    temp = getNodeFromLR(arr,m,l,r, st, 2 * cur + 1, 2 * cur + 2);;
-    return temp;
-}
-
-struct stNode getNodeFromLR(int *arr,int m,int l,int r,struct stNode *st, int lc, int rc)
+struct stNode getNodeFromLRB(int *arr,int m,int l,int r,struct stNode *st, int lc, int rc)
 {
     struct stNode temp;
     temp.fqLtMost = st[lc].fqLtMost ;
-    if(arr[l]==arr[m+1]) temp.fqLtMost =+ st[rc].fqLtMost;
+    if(arr[l]==arr[m+1]) temp.fqLtMost +=st[rc].fqLtMost;
     temp.fqRtMost = st[rc].fqRtMost;
-    if(arr[m]==arr[r+1]) temp.fqRtMost =+ st[lc].fqRtMost;
+    if(arr[m]==arr[r]) temp.fqRtMost += st[lc].fqRtMost;
     temp.fqMax = max(st[lc].fqMax,st[rc].fqMax);
     if(arr[m]==arr[m+1]) temp.fqMax = max(st[lc].fqRtMost+st[rc].fqLtMost, temp.fqMax);
     
     return temp;
 }
+struct stNode getNodeFromLR(int *arr,int m,int l,int r,struct stNode *st, struct stNode lc, struct stNode rc)
+{
+    struct stNode temp;
+    temp.fqLtMost = lc.fqLtMost ;
+    if(arr[l]==arr[m+1]) temp.fqLtMost += rc.fqLtMost;
+    temp.fqRtMost = rc.fqRtMost;
+    if(arr[m]==arr[r]) temp.fqRtMost += lc.fqRtMost;
+    temp.fqMax = max(lc.fqMax,rc.fqMax);
+    if(arr[m]==arr[m+1]) temp.fqMax = max(lc.fqRtMost+rc.fqLtMost, temp.fqMax);
+    
+    return temp;
+}
+struct stNode RMQ(int *arr,struct stNode *st, int cur, int ql, int qr, int l, int r)
+{
+    struct stNode temp;
+    temp.fqLtMost = temp.fqRtMost = temp.fqMax = 0;
+    if (l > qr || r < ql )
+        return temp;
+    if (l >= ql && r <= qr)
+    {
+        return st[cur];
+    }
+    int m = (l + r) / 2;
+    
+    struct stNode lst = RMQ(arr,st, 2 * cur + 1, ql, qr, l, m);
+    struct stNode rst = RMQ(arr,st, 2 * cur + 2, ql, qr, m + 1, r);
+
+    temp = getNodeFromLR(arr,m,l,r, st, lst, rst);
+    return temp;
+}
+
+
 void build(int *arr, struct stNode *st, int cur, int l, int r)
 {
+    if(l>r)
+    return; 
     if (l == r)
     {
         st[cur].fqLtMost = 1;
@@ -74,7 +74,7 @@ void build(int *arr, struct stNode *st, int cur, int l, int r)
         build(arr, st, 2 * cur + 1, l, m);
         build(arr, st, 2 * cur + 2, m + 1, r);
         
-        st[cur] = getNodeFromLR(arr,m,l,r, st, 2 * cur + 1, 2 * cur + 2);
+        st[cur] = getNodeFromLRB(arr,m,l,r, st, 2 * cur + 1, 2 * cur + 2);
         
     }
 }
@@ -82,8 +82,10 @@ void build(int *arr, struct stNode *st, int cur, int l, int r)
 int main()
 {
     fastIO;
-    int n;
-    cin >> n;
+    int n,q;
+    cin >> n ;
+    while(n){
+    cin >> q;        
     int arr[n];
     for (ll i = 0; i < n; i++)
         cin >> arr[i];
@@ -91,15 +93,16 @@ int main()
 
     build(arr, st, 0, 0, n - 1);
 
-    int q;
-    cin >> q;
     while (q--)
     {
-        ll x, y;
-        cin >> x >> y;
+        int x, y;
+        cin >> x ;
+        if(x==0) break;
+        cin >> y;
         cout << RMQ(arr,st, 0, x - 1, y - 1, 0, n - 1).fqMax << endl;
-        ;
+        
     }
-
+    cin >> n;
+}
     return 0;
 }
